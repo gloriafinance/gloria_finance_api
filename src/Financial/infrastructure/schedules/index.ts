baf3684a984c6cc2c9c834f5151a6fabc7f44c71
abejarano <programador.angel@gmail.com>
2026-01-02 09:20:33 -0300

    fix: GenerateFinancialMonthsService and isLastDayMonth

--- src/Financial/infrastructure/schedules/index.ts
+++ src/Financial/infrastructure/schedules/index.ts
@@ -1,18 +1,12 @@
+import * as dayjs from "dayjs"
 import { DateBR } from "@/Shared/helpers"
 import * as cron from "node-cron"
 import { CloseFinancialMonthService } from "../services/CloseFinancialMonth.service"
 import { GenerateFinancialMonthsService } from "@/Financial/infrastructure/services/GenerateFinancialMonths.service"
 
 const isLastDayMonth = (): boolean => {
-  const currencyDate = DateBR()
-
-  const today = currencyDate.getDate()
-  const currentMonth = currencyDate.getMonth() // Los meses comienzan desde 0 (enero = 0)
-  const currentYear = currencyDate.getFullYear()
-
-  const dateTomorrow = new Date(currentYear, currentMonth, today + 1)
-
-  return dateTomorrow.getMonth() !== currentMonth
+  const now = dayjs.tz("America/Sao_Paulo")
+  return now.add(1, "day").month() !== now.month()
 }
 
 const isDecember = () => DateBR().getMonth() === 11
@@ -26,10 +20,11 @@ export const FinancialSchedules = () => {
         await CloseFinancialMonthService()
       }
 
-      if (isDecember) {
+      if (isDecember()) {
         GenerateFinancialMonthsService()
       }
     },
+
     {
       timezone: "America/Sao_Paulo",
     }
