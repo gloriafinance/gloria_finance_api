73ccafecc5d6738cd68ddd7be8a3f502f1f9c3b3
abejarano <programador.angel@gmail.com>
2026-01-02 10:21:54 -0300

    feat: endpoint for generate financial months

--- src/ConsolidatedFinancial/infrastructure/http/controllers/FinancialMonth.controller.ts
+++ src/ConsolidatedFinancial/infrastructure/http/controllers/FinancialMonth.controller.ts
@@ -3,11 +3,20 @@ import domainResponse from "@/Shared/helpers/domainResponse"
 import { UpdateFinancialMonth } from "@/ConsolidatedFinancial/applications/FinancialMonth"
 import { FinancialYearMongoRepository } from "@/ConsolidatedFinancial/infrastructure"
 import { HttpStatus } from "@/Shared/domain"
-import { Controller, Get, Patch, Use } from "@abejarano/ts-express-server"
+import {
+  Body,
+  Controller,
+  Get,
+  Patch,
+  Post,
+  Use,
+} from "@abejarano/ts-express-server"
 import { Can, PermissionMiddleware } from "@/Shared/infrastructure"
 import { ListFinancialMonth } from "@/ConsolidatedFinancial/applications"
 import { ListFinancialMonthRequest } from "@/ConsolidatedFinancial/domain"
 import FinancialMonthValidator from "../validator/FinancialMonth.validator"
+import { GenerateFinanceRecordReport } from "@/Financial/applications"
+import { GenerateFinancialMonthsService } from "../../services/GenerateFinancialMonths.service"
 
 @Controller("/api/v1/finance/consolidate")
 export class FinancialMonthController {
@@ -47,6 +56,20 @@ export class FinancialMonthController {
       } as ListFinancialMonthRequest)
 
       res.status(HttpStatus.OK).send(list)
+    } catch (e) {
+      domainResponse(e, res)
+    }
+  }
+
+  @Post("/")
+  @Use([PermissionMiddleware, Can("consolidated_financial", "generate_months")])
+  async generate(@Body() req: { year: number }, res: Response) {
+    try {
+      await GenerateFinancialMonthsService(req.year)
+
+      res
+        .status(HttpStatus.OK)
+        .send({ message: "Financial month generate successfully" })
     } catch (e) {
       domainResponse(e, res)
     }
